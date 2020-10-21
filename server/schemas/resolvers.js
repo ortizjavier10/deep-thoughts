6e01f8caa8cec0bd0f520ce137abc51ab55405c3
JAVI <ortiz.javier10@gmail.com>
2020-10-21 18:50:14 -0400

    graphql queries

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,30 @@
+const { User,  Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World!';
-        }
+      // get all users
+      users: async () => {
+        return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('toughts');
+      },      
+      // get a user by username
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+      },
+      thoughts: async (parent, { username }) => {
+          // ternary operator to check if username exists
+        const params = username ? { username } : {};
+        return Thought.find(params).sort({ createdAt: -1 });
+      },
+      // we destructure the _id argument value and place it into our .findOne() method to look up a single thought by its _id
+      thought: async (parent, { _id }) => {
+          return Thought.findOne({ _id });
+      }
     }
-};
-
+  };
 module.exports = resolvers;
--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -74,3 +74,4 @@ db.once('open', async () => {
   console.log('all done!');
   process.exit(0);
 });
+
